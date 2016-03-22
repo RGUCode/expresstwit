cc5f2ed12c804d9714b792badfe623da2c28321f
John <wildfireone@gmail.com>
2016-03-22 21:44:47 +0000

    testing socket

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('/eu', eu);
+//app.use('/eu', eu);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
