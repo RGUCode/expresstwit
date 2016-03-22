7128c6807d570a2373e2c6a32f55396bf11831ee
John <wildfireone@gmail.com>
2016-03-22 22:30:44 +0000

    testing socket

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 var express = require('express');
+var socket_io    = require( "socket.io" );
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
+var routes = require('./routes/index')(io);
 var users = require('./routes/users');
 
-var app = express();
-var io = require('./lib/io')(server);
+// Express
+var app          = express();
+
+// Socket.io
+var io           = socket_io();
+app.io           = io;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,13 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+module.exports = function(io) {
+    var app = require('express');
+    var router = app.Router();
 
+    io.on('connection', function(socket) {
+        (...)
+    });
 
-module.exports = router;
+    return router;
+};
