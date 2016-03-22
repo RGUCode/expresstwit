14622daf01f7242d53facbd561b860af0a5d0a79
John <wildfireone@gmail.com>
2016-03-22 21:12:37 +0000

    setup eu map

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var eu = require('./routes/eu');
 
 var app = express();
 
