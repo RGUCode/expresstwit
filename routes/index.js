faf685de1d9adc051061aa5c6b8be5efdfad075f
John <wildfireone@gmail.com>
2016-03-22 22:37:41 +0000

    testing socket

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,18 @@
 var express = require('express');
-var router = express.Router();
-var io = router.io;
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-io.on('connection', function(socket) {
+router.io.on('connection', function(socket) {
 
 });
 
 module.exports = function(io) {
     var app = require('express');
     var router = app.Router();
-    this.io = io;
+
 
 
     return router;
