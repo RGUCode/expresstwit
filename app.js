84f22058633bd3e246f07838c7bab831bfe8a9c8
John <wildfireone@gmail.com>
2016-03-22 21:01:04 +0000

    setup eu map

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-
+var io = require('socket.io')(app);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -24,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/eu', eu);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
