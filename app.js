a5d0345a1298739a044c4ed85d550a6aa3bab6c0
John <wildfireone@gmail.com>
2016-03-22 23:17:48 +0000

    testing socket

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ var users = require('./routes/users');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
+app.use(express.static('public'));
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
