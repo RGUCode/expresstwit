df2f3b5c793b80f52ae1115e75c8f0254330408f
John <wildfireone@gmail.com>
2016-03-22 21:11:14 +0000

    setup eu map

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-var io = require('socket.io')(app);
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
