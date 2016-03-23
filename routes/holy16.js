646dc33af44995b33f5a1cc5a6c04700ab307069
John <wildfireone@gmail.com>
2016-03-23 22:57:41 +0000

    added graphs skeleton

--- routes/holy16.js
+++ routes/holy16.js
@@ -101,7 +101,7 @@ module.exports = function(io) {
                  io.emit('geo', data);
                }
                //var tweetdata = {name: tweet.user.name}
-                 io.emit('tweet',tweet.name);
+                 io.emit('tweet',tweet.user.name);
                  //console.log(counter++);
 
               }
@@ -115,7 +115,7 @@ module.exports = function(io) {
         client.stream('statuses/filter', {track: 'leadersdebate,holyrood16,holyrood2016,sp16,scotland16'},  function(stream){
 
           stream.on('data', function(tweet) {
-            io.emit('tweet', tweet.name);
+            io.emit('tweet', tweet.user.name);
             MongoClient.connect(mongoURL, function(err, db) {
               assert.equal(null, err);
               insertDocument(db,tweet, function() {
