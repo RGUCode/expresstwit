c5703dded0f068e2db3ed067984a0426878a24d9
John <wildfireone@gmail.com>
2016-03-23 18:53:39 +0000

    really removed eu

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,8 @@
-module.exports = function(io) {
-    var app = require('express');
-    var router = app.Router();
 
-    /* GET home page. */
-    router.get('/', function(req, res, next) {
-      res.render('index', { title: 'Express' });
-    });
-
-
-    io.on('connection', function(socket) {
-      socket.emit('welcome', { message: 'Welcome! tweets tracked', id: socket.id });
-    });
-
-    return router;
-}
+var app = require('express');
+var router = app.Router();
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+module.exports = app;
