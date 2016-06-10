0548d9a6e02ae14daa082e40dc63776402fd0afb
John <wildfireone@gmail.com>
2016-06-10 15:05:57 +0100

    changed how everything works....sorry

---
+++ twitscraper2.js
@@ -0,0 +1,125 @@
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var ObjectId = require('mongodb').ObjectID;
+var mongoURL = 'mongodb://localhost:27017/tweets';
+var Twitter = require('twitter');
+var tweettools = require('./routes/tools/TweetToNeo');
+var socket_io    = require( "socket.io" );
+var counter = 0;
+var io           = socket_io();
+var client = new Twitter({
+  consumer_key: 'hhZTif85m9t7Q9aqEUSRxdSwI',
+  consumer_secret: 'vnqq3G2hG887KZyeLa0wXkmN19Bn4N8a3CGAf16MBN8TVBeEcQ',
+  access_token_key: '14812487-BzawCSPGaNdrJvoa1VrtJ3DbMFgr7nMiAV3x21rzX',
+  access_token_secret: 'JqfvcFqBTMYS5RbFeTJ0ai37BEd1QRTZ7npe14bB06TZo',
+});
+//console.log(consumer_key +" : "+ consumer_secret +" : "+ access_token_key +" : "+ z)
+/**
+ * Stream statuses filtered by keyword
+ * number of tweets per second depends on topic popularity
+ **/
+
+client.stream('statuses/filter', {track: 'eureferendum,euref,brexit,no2eu,notoeu,betteroffout,voteout,britainout,leaveeu,voteleave,beleave,leaveeu,yes2eu,yestoeu,betteroffin,votein,ukineu,bremain,strongerin,leadnotleave,voteremain'},  function(stream){
+
+  stream.on('data', function(tweet) {
+    tweettools.processTweet(tweet, io);
+    var geodata;
+    var tweettext = tweet.text.toLowerCase();
+    if(tweetSearch(tweettext, remainTags)){
+      io.emit('tweet', {tweet:tweet.user.name, vote : 'stay' });
+      if(tweet.geo !=null){
+        data = { cord : tweet.geo.coordinates , ineu : 'true' };
+        io.emit('eugeo', data);
+      }
+      //stayc++;
+    }
+    if(tweetSearch(tweettext, leaveTags)){
+      io.emit('tweet', {tweet:tweet.user.name, vote : 'leave' });
+      if(tweet.geo !=null){
+        data = { cord : tweet.geo.coordinates , outeu : 'true' };
+        io.emit('eugeo', data);
+        }
+      //leavec++;
+    }
+
+    MongoClient.connect(mongoURL, function(err, db) {
+      assert.equal(null, err);
+      //add tweet to mongo
+      insertDocument(db,tweet, function() {});
+      //incrementout totals
+      incrementCount(db,tweet);
+      //emit all the stats and close
+      emitStatsCount(db,function(){db.close();});
+
+    });
+  });
+
+  stream.on('error', function(error) {
+    console.log(error);
+  });
+});
+const leaveTags = ['brexit','no2eu','notoeu','betteroffout','voteout','britainout','leaveeu','voteleave','beleave'];
+const remainTags = ['bremain','yes2eu','yestoeu','betteroffin','votein','ukineu','strongerin','leadnotleave','voteremain'];
+
+
+var tweetSearch = function(string, strings){
+  for(var i=0; i<strings.length;i++) {
+      if(string.indexOf(strings[i])>0){
+        //console.log(entry);
+        return true;
+      }
+      return false;
+    };
+}
+
+var insertDocument = function(db, newtweet, callback) {
+    db.collection('euref').insertOne( newtweet, function(err, result) {
+      assert.equal(err, null);
+      //console.log("Inserted a document into the tweets collection.");
+      callback();
+    });
+};
+
+var incrementCount = function(db,tweet) {
+  var countin=0;
+  var countout =0;
+    var tweettext = tweet.text.toLowerCase();
+    if(tweetSearch(tweettext, leaveTags)){
+      countout=1;
+    }
+    if(tweetSearch(tweettext, remainTags)){
+      countin=1;
+    }
+    db.collection('eucounts').update({},{$inc:{"count.in":countin, "count.out":countout}},function(err, result){
+      counter ++;
+      if((counter % 10000) == 0){
+        console.log(counter);
+      }
+      emitCount(db,function(){});
+    });
+}
+var emitCount = function(db,callback){
+  db.collection('eucounts').find({}).toArray(function(err, docs) {
+    var inoutcount = docs[0];
+    io.emit('status',
+    {
+      incount: inoutcount.in,
+      outcount: inoutcount.out
+    });
+  });
+}
+
+var emitStatsCount = function(db,callback){
+  db.collection(COLLECTION).count(function(err, count){
+    io.emit('welcome',
+    {
+      count: count,
+      tweet: tweet.text,
+      time: tweet.created_at,
+      message: '<p>Currently '+count+' tweets tracked</p>'+
+             '<p>Last Tweet :'+tweet.text+'</p>'+
+             '<p>@'+tweet.created_at+'</p>'
+    });
+    callback();
+  });
+}
