b3ad0f72923c9b90c8384e3a2696025094830f4e
John <wildfireone@gmail.com>
2016-06-14 21:02:30 +0100

    updated neodynamic for emited

--- routes/tools/TweetToNeo.js
+++ routes/tools/TweetToNeo.js
@@ -226,7 +226,7 @@ function storeTweet(t) {
     //}
 
 
-    //io.emit('neodata',{'cypher':'runnig cypher'});
+    io.emit('neodata',{'cypher':'runnig cypher'});
     //queries.push(tweetText);
     runCypherQueryMatch(tweetText, function (err, resp) {
       //ru the query against neo4J- Basically add the tweets as a node and associated links
